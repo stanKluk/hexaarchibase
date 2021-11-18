1530f5add3044c3e9f9055dec51a932055191993
stanislas.klukowski@gmail.com <!Jb3cpj1988>
2021-11-18 19:48:22 +0100

    Initial commit : spring initializr dependencies

---
+++ src/main/java/com/fsk/hexaarchi/hexaarchibase/HexaarchibaseApplication.java
@@ -0,0 +1,13 @@
+package com.fsk.hexaarchi.hexaarchibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaarchibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexaarchibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsk/hexaarchi/hexaarchibase/HexaarchibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsk.hexaarchi.hexaarchibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaarchibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
